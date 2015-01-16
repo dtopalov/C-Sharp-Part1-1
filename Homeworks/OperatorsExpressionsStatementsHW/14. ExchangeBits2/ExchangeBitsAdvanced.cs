ef9805b3da0366b07c97f17b7a62cb2d3583518a
dtopalov <dimiter.topalov79@gmail.com>
2015-01-16 17:14:01 +0200

    Updated Problem 16.** Bit Exchange (Advanced)
    Improved to show results Overlapping and Out of range.

--- Homeworks/OperatorsExpressionsStatementsHW/14. ExchangeBits2/ExchangeBitsAdvanced.cs
+++ Homeworks/OperatorsExpressionsStatementsHW/14. ExchangeBits2/ExchangeBitsAdvanced.cs
@@ -10,21 +10,36 @@ class ExchangeBitsAdvanced
     static void Main(string[] args)
     {
         Console.Write("Please enter a positive integer n: ");
-        uint input = Convert.ToUInt32(Console.ReadLine());
+        uint input = 0;
+        bool isInRange = uint.TryParse(Console.ReadLine(), out input);
         Console.Write("How many bits to exchange? ");
-        int bitsNumber = Convert.ToInt32(Console.ReadLine());
+        int numberOfBits = Convert.ToInt32(Console.ReadLine());
         Console.Write("First sequence of bits starting bit number: ");
         int firstSequenceStart = Convert.ToInt32(Console.ReadLine());
         Console.Write("Second sequence of bits starting bit number: ");
         int secondSequenceStart = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("n: " + input + "; n(binary): " + Convert.ToString(input, 2).PadLeft(32, '0'));
-        for (int i = firstSequenceStart; i < (firstSequenceStart + bitsNumber); i++)
+        if (isInRange == false || Math.Min(firstSequenceStart, secondSequenceStart) < 0 || Math.Max(firstSequenceStart, secondSequenceStart) + numberOfBits > 31)
         {
-            uint bit1 = (input >> i) & 1;
-            uint bit2 = (input >> ((secondSequenceStart - firstSequenceStart) + i)) & 1;
-            input = (uint)(input & (~(1 << ((secondSequenceStart - firstSequenceStart) + i))) | (bit1 << ((secondSequenceStart - firstSequenceStart) + i)));
-            input = (uint)(input & (~(1 << i)) | (bit2 << i));
+            Console.WriteLine("out of range");
         }
-        Console.WriteLine("result: " + input + "; result(binary): " + Convert.ToString(input, 2).PadLeft(32, '0'));
+        else
+        {
+            if (Math.Min(firstSequenceStart, secondSequenceStart) + numberOfBits > Math.Max(firstSequenceStart, secondSequenceStart))
+            {
+                Console.WriteLine("Overlapping");
+            }
+            else
+            {
+                Console.WriteLine("n: " + input + "; n(binary): " + Convert.ToString(input, 2).PadLeft(32, '0'));
+                for (int i = firstSequenceStart; i < (firstSequenceStart + numberOfBits); i++)
+                {
+                    uint bit1 = (input >> i) & 1;
+                    uint bit2 = (input >> ((secondSequenceStart - firstSequenceStart) + i)) & 1;
+                    input = (uint)(input & (~(1 << ((secondSequenceStart - firstSequenceStart) + i))) | (bit1 << ((secondSequenceStart - firstSequenceStart) + i)));
+                    input = (uint)(input & (~(1 << i)) | (bit2 << i));
+                }
+                Console.WriteLine("result: " + input + "; result(binary): " + Convert.ToString(input, 2).PadLeft(32, '0'));
+            }
+        }
     }
 }
